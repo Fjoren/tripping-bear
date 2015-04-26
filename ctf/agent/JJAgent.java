6f576102785512a7b4661fd4f1da77d8949cac8b
Fjoren <braden@asobu.org>
2015-04-26 15:25:23 -0500

    first commit

--- ctf/agent/JJAgent.java
+++ ctf/agent/JJAgent.java
@@ -4,58 +4,74 @@ import ctf.common.AgentEnvironment;
 import ctf.agent.Agent;
 
 import ctf.common.AgentAction;
+//this is the jobs you can have
+enum Job {MAPPING, RANDOM_MOVES, FIND_MAP_SIZE, GUARDING, ATTACKING}
 
-enum Job {IDLE, FIND_MAP_SIZE}
-
+//Jeffrey Jennifer Agent
 public class JJAgent extends Agent {
     
-    static int nextID = 0;
+    static Board board;
+    //which one is which
     int id;
+    //this instaniazies it 
     boolean firstMove = true;
-    Job currentJob = Job.IDLE;
+    ArrayList<Job> currentJobs = new ArrayList<Job>();
+    ArrayList<AgentAction> history = new ArrayList<AgentAction>(); 
     int counter = 0;
-
+    int xDisplacement = 0;
+    int yDisplacement = 0;
+    
+    //top one gets size
+    //both are constantly mapping (updates the map) 
+    //
     public int getMove(AgentEnvironment inEnvironment) {
-    
         if (firstMove) {
-           id = nextID;
-           nextID++;
-           firstMove = false; 
+            if (inEnvironment.isFlagSouth(AgentEnvironment.OUR_TEAM, false))
+                id = 0;
+            if (inEnvironment.isFlagNorth(AgentEnvironment.OUR_TEAM, false))
+                id = 1;
+            if (id == 0) {
+                board = new Board();
+                currentJobs.add(Job.MAPPING);
+                currentJobs.add(Job.FIND_MAP_SIZE);
+            }
+            if (id == 1) {
+                currentJobs.add(Job.RANDOM_MOVES);
+                currentJobs.add(Job.MAPPING);
+            }
+            firstMove = false;
+            if (nextId == 2)
+                nextId = 0;
         }
 
-        if (id == nextID - 2)
-            currentJob = Job.FIND_MAP_SIZE;
-
-        switch(currentJob) {
-        
-            case FIND_MAP_SIZE:
-                if (inEnvironment.isFlagSouth(inEnvironment.OUR_TEAM, false)) {
-                    counter++;
-                    return AgentAction.MOVE_SOUTH;
-                }
-                else if (inEnvironment.isFlagSouth(inEnvironment.OUR_TEAM, true)) {
-                    counter++;
-                    System.err.println(counter);
-                    currentJob = Job.IDLE;
-                }
-        
+        //Non Final Jobs
+        if (currentJobs.includes(Job.MAPPING)) {
+            
         }
 
-        return AgentAction.MOVE_WEST;
-    
+        //Final Jobs
+        
+
     }
 
+    //tile on board, stores any information you want it to have
     private class BoardTile {
     
         int hasBomb;
         boolean blocked;
-
+        int hasAgent;
+        boolean unknown;
     
     }
 
     private class Board {
     
-        BoardTile[][] board = new BoardTile[10][10];
+        ArrayList<ArrayList<BoardTile>> board = new ArrayList<ArrayList<BoardTile>>();
+        boolean complete = false;
+
+        void makeBigger();
+        void completed() {complete = true;}
+        boolean isComplete() {return complete;}
 
     }
 
